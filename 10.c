33bec9caf7b23e15217458718ae671238ea79c1d
Maria Clara <113727788+mariaclara78900@users.noreply.github.com>
2023-04-22 13:40:53 -0300

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	char nome[50];
+	int idade;
+	
+	printf("Digite seu nome: \n");
+	gets(nome);
+	
+	printf("Digite sua idade: \n");
+	scanf("%d", &idade);
+	
+	printf("Ola %s, seja bem-vindo(a)! Parabens pelos seus %d anos! \n", nome, idade);
+	
+	system("pause");
+}
+
